550190fc229de3fed6baa1db19bc0da9845591cd
Corey Hansen <chansen1532@hotmail.com>
2017-04-15 21:42:11 -0700

    h

---
+++ includes/wolf3d.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wolf3d.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chansen <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/28 17:59:00 by chansen           #+#    #+#             */
+/*   Updated: 2016/11/28 17:59:03 by chansen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WOLF3D_H
+# define WOLF3D_H
+
+# include "./../libft/includes"
+# include "./../minilibx/mlx.h"
+
+# include <fcntl.h>
+# include <math.h>
+
+typedef struct	s_;
+
+
+typedef struct	s_p1
+{
+	void		*dope;
+}				t_p1;
+
+typedef struct	s_env
+{
+	void		*mlx;
+}				t_env;
+
+
+#endif
